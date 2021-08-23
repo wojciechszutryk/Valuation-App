2bf43091f13b7fe51957953683a4209420ed8305
szutryk <wojtekszutryk@gmail.com>
2021-08-23 19:08:32 +0200

    add translaction handling functions, remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
+import {useTranslation} from "react-i18next";
+import LanguageSwitcher from "./components/LanguageSwitcher";
+import "./i18n"
 
 function App() {
+  const {t} = useTranslation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LanguageSwitcher/>
+      {t("Witam")}
     </div>
   );
 }
---
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -0,0 +1,24 @@
+import React, {useEffect} from 'react';
+import {useTranslation} from "react-i18next";
+
+const LanguageSwitcher = () => {
+    const { i18n } = useTranslation();
+
+    const changeLanguage = React.useCallback((lng) =>  {
+        i18n.changeLanguage(lng);
+    },[i18n]);
+
+    useEffect(()=>{
+        changeLanguage(navigator.language);
+    },[changeLanguage])
+
+    return (
+        <div>
+            <button onClick={() => changeLanguage('en')}>En</button>
+            <button onClick={() => changeLanguage('de')}>De</button>
+            <button onClick={() => changeLanguage('pl')}>Pl</button>
+        </div>
+    );
+};
+
+export default LanguageSwitcher;
