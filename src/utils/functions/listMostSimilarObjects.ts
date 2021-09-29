0ac5f9395d6aa5570ecaff96414218d5c4e40345
szutryk <wojtekszutryk@gmail.com>
2021-09-29 13:49:28 +0200

    finish listMostSimilarObjects.ts

--- src/utils/functions/listMostSimilarObjects.ts
+++ src/utils/functions/listMostSimilarObjects.ts
@@ -1,143 +1,17 @@
+import { findMostSimilarObject } from './findMostSimilarObject'
+
 export const listMostSimilarObjects = (
-    objects: { [key: string]: number }[] = [
-        {
-            Lokalizacja: 2,
-            'Sąsiedztwo i otoczenie': 1,
-            'Kształt i wielokość działki': 2,
-            Uzbrojenie: 3,
-            Dojazd: 2,
-        },
-        {
-            Lokalizacja: 3,
-            'Sąsiedztwo i otoczenie': 3,
-            'Kształt i wielokość działki': 4,
-            Uzbrojenie: 4,
-            Dojazd: 3,
-        },
-        {
-            Lokalizacja: 3,
-            'Sąsiedztwo i otoczenie': 3,
-            'Kształt i wielokość działki': 4,
-            Uzbrojenie: 3,
-            Dojazd: 3,
-        },
-        {
-            Lokalizacja: 3,
-            'Sąsiedztwo i otoczenie': 3,
-            'Kształt i wielokość działki': 4,
-            Uzbrojenie: 1,
-            Dojazd: 3,
-        },
-        {
-            Lokalizacja: 4,
-            'Sąsiedztwo i otoczenie': 4,
-            'Kształt i wielokość działki': 4,
-            Uzbrojenie: 4,
-            Dojazd: 4,
-        },
-        {
-            Lokalizacja: 2,
-            'Sąsiedztwo i otoczenie': 1,
-            'Kształt i wielokość działki': 3,
-            Uzbrojenie: 2,
-            Dojazd: 1,
-        },
-        {
-            Lokalizacja: 2,
-            'Sąsiedztwo i otoczenie': 3,
-            'Kształt i wielokość działki': 4,
-            Uzbrojenie: 2,
-            Dojazd: 3,
-        },
-        {
-            Lokalizacja: 2,
-            'Sąsiedztwo i otoczenie': 2,
-            'Kształt i wielokość działki': 3,
-            Uzbrojenie: 2,
-            Dojazd: 2,
-        },
-        {
-            Lokalizacja: 2,
-            'Sąsiedztwo i otoczenie': 2,
-            'Kształt i wielokość działki': 3,
-            Uzbrojenie: 2,
-            Dojazd: 3,
-        },
-        {
-            Lokalizacja: 2,
-            'Sąsiedztwo i otoczenie': 2,
-            'Kształt i wielokość działki': 4,
-            Uzbrojenie: 3,
-            Dojazd: 3,
-        },
-        {
-            Lokalizacja: 2,
-            'Sąsiedztwo i otoczenie': 2,
-            'Kształt i wielokość działki': 4,
-            Uzbrojenie: 2,
-            Dojazd: 3,
-        },
-        {
-            Lokalizacja: 3,
-            'Sąsiedztwo i otoczenie': 3,
-            'Kształt i wielokość działki': 4,
-            Uzbrojenie: 3,
-            Dojazd: 3,
-        },
-        {
-            Lokalizacja: 3,
-            'Sąsiedztwo i otoczenie': 2,
-            'Kształt i wielokość działki': 4,
-            Uzbrojenie: 2,
-            Dojazd: 3,
-        },
-        {
-            Lokalizacja: 3,
-            'Sąsiedztwo i otoczenie': 2,
-            'Kształt i wielokość działki': 4,
-            Uzbrojenie: 2,
-            Dojazd: 1,
-        },
-        {
-            Lokalizacja: 2,
-            'Sąsiedztwo i otoczenie': 1,
-            'Kształt i wielokość działki': 2,
-            Uzbrojenie: 2,
-            Dojazd: 2,
-        },
-    ],
-    object: { [key: string]: number } = {
-        Lokalizacja: 2,
-        'Sąsiedztwo i otoczenie': 1,
-        'Kształt i wielokość działki': 2,
-        Uzbrojenie: 3,
-        Dojazd: 2,
-    }
+    objects: { [key: string]: number }[],
+    object: { [key: string]: number }
 ) => {
-    const IndexesOfObjectsWithOneNotEqualValue: number[][] = []
-    for (let i = 0; i < objects.length; i++) {
-        const firstObject: { [key: string]: number } = objects[i]
-        for (let j = 0; j < objects.length; j++) {
-            // const secondObject: { [key: string]: number } = objects[j]
-            // if (secondObject[searchKey] !== firstObject[searchKey]) {
-            //     const firstObjectCopy = Object.assign({}, firstObject)
-            //     delete firstObjectCopy[searchKey]
-            //     const secondObjectCopy = Object.assign({}, secondObject)
-            //     delete secondObjectCopy[searchKey]
-            //     if (
-            //         JSON.stringify(secondObjectCopy) ===
-            //         JSON.stringify(firstObjectCopy)
-            //     ) {
-            //         i < j
-            //             ? IndexesOfObjectsWithOneNotEqualValue.push([i, j])
-            //             : IndexesOfObjectsWithOneNotEqualValue.push([j, i])
-            //     }
-            // }
-        }
+    const objectsLength = objects.length
+    const numberOfIndexesToFind = Math.floor(objectsLength / 4) + 1
+    const mostSimilarObjectsIndexes: number[] = []
+
+    while (mostSimilarObjectsIndexes.length < numberOfIndexesToFind) {
+        const mostSimilarObjectIndex = findMostSimilarObject(objects, object)
+        mostSimilarObjectsIndexes.push(mostSimilarObjectIndex)
+        objects.splice(mostSimilarObjectIndex, 1)
     }
-    return Array.from(
-        new Set(
-            IndexesOfObjectsWithOneNotEqualValue.map((e) => JSON.stringify(e))
-        )
-    ).map((e) => JSON.parse(e))
+    return mostSimilarObjectsIndexes
 }
